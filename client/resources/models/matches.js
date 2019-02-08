febbf03d629ff0ada0d16ee7231cbeb547023cf5
Dylanr57 <dylanr574423@gmail.com>
2019-02-08 10:39:48 -0800

    added models and added email to user schema

---
+++ client/resources/models/matches.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+    var Matches = sequelize.define("Post", {
+        user1Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user2Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user1Waved: {
+            type: DataTypes.Boolean
+        },
+        user2Waved: {
+            type: DataTypes.Boolean
+        },
+        user1Active: {
+            type: DataTypes.Boolean
+        },
+        user2Active: {
+            type: DataTypes.Boolean
+        },
+        canCall: {
+            type: DataTypes.Boolean
+        }
+    });
+    return Matches;
+}
---
+++ client/resources/models/user.js
@@ -0,0 +1,49 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("Post", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2],
+                is: ["^[a-z]+$",'i']
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        photo: {
+            type: DataTypes.BLOB('long'),
+            allowNull: false
+        },
+        sex: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        interests: {
+            type: DataTypes.STRING
+        },
+        findMan: {
+            type: DataTypes.BOOLEAN
+        },
+        findWoman: {
+            type: DataTypes.BOOLEAN
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+
+    });
+    return User;
+}
