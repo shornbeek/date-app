499cfe4eb441bf7a92f70a66d3901cb3a18ce2ea
Dylanr57 <dylanr574423@gmail.com>
2019-02-12 13:41:06 -0800

    filtered by gender and preference

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,94 @@
 const {User} = require("./../models");
 console.log(User);
-function index(req, res) {
-    User.findAll({}).then(result => {
-        res.json(result);
-    }).catch(err => {
-        if (err) throw err;
-    });
-}
-function ifExists(req,res) {
-    User.count({
-        where: {
-            id: req.params.id
-        }
-    }).then(result => {
-        if(result === 0){
-            res.send(false);
-        }else{
-            res.send(true);
-        }
-    }).catch(err => {
-        if (err) throw err;
-    });
-}
-
 module.exports = {
-    index, ifExists
+
+    findUser(req, res) {
+        User.findAll({
+            where: {
+                id: req.params.id
+            }
+        }).then(result => {
+            res.json(result);
+        }).catch(err => {
+            if (err) throw err;
+        });
+    },
+
+    ifExists(req,res) {
+        User.count({
+            where: {
+                id: req.params.id
+            }
+        }).then(result => {
+            if(result === 0){
+                res.send(false);
+            }else{
+                res.send(true);
+            }
+        }).catch(err => {
+            if (err) throw err;
+        });
+    },
+
+    findByGender(req,res) {
+        User.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(result1 => {
+            if(result1.findMan && result1.findWoman){
+                User.findAll({
+                }).then(result2 => {
+                    let noSelf = result2.filter(user => user.id !== req.params.id);
+                    let final = [];
+                    noSelf.forEach(match => {
+                        if ((match.findMan && !result1.sex) || (match.findWoman && result1.sex)){
+                            final.push(match);
+                        }
+                    });
+                    res.json(final);
+                }).catch(err => {
+                    if (err) throw err;
+                });
+            } else if (result1.findMan){
+                User.findAll({
+                    where: {
+                        sex: false
+                    }
+                }).then(result2 => {
+                    let noSelf = result2.filter(user => user.id !== req.params.id);
+                    let final = [];
+                    noSelf.forEach(match => {
+                        if ((match.findMan && !result1.sex) || (match.findWoman && result1.sex)){
+                            final.push(match);
+                        }
+                    });
+                    res.json(final);
+                }).catch(err => {
+                    if (err) throw err;
+                });
+            } else {
+                User.findAll({
+                    where: {
+                        sex: true
+                    }
+                }).then(result2 => {
+                    let noSelf = result2.filter(user => user.id !== req.params.id);
+                    let final = [];
+                    noSelf.forEach(match => {
+                        if ((match.findMan && !result1.sex) || (match.findWoman && result1.sex)){
+                            final.push(match);
+                        }
+                    });
+                    res.json(final);
+                }).catch(err => {
+                    if (err) throw err;
+                });
+            }
+        }).catch(err => {
+            if (err) throw err;
+        });
+    }
+
+
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const db = require("./../controllers/users");
 
-router.get("/users", db.index);
+router.get("/users/:id", db.findUser);
 
 router.get("/users/if/:id", db.ifExists);
+
+router.get("/users/genderfind/:id", db.findByGender);
 
 module.exports = router;
