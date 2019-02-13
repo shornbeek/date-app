0ddc562d3ec2a2d9363a3ce7c42d99cf6e176306
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-13 13:00:02 -0800

    help

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mysql2 = require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+let db = require('./models');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(require("./routes/api-routes.js"));
 
-app.listen(PORT, ()=>{
-    console.log("App listenign on " + PORT);
-});
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
