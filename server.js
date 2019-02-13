d94706785cb6da1a65e08f03f22d1c208362d984
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-13 13:22:59 -0800

    server update

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const mysql2 = require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 3001;
 let db = require('./models');
+const sequelize = require("sequelize");
+const server = require("http").createServer(app);
+
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(require("./routes/api-routes.js"));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
 db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
+    server.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
   });
