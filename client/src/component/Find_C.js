c74c75845c2a7e1b1136a19a8e9a25878f70449c
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-13 15:07:15 -0800

    styling added

--- client/src/component/Find_C.js
+++ client/src/component/Find_C.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
 import Nav_C from "./Nav_C";
-import "./styleFind.css";
-// import Nav_C from "./component/Nav_C";
+import "../style/Find_C.css";
+import Profile_C from './Profile_C';
+import LoveBtn_C from './loveBtn_C';
+import DislikeBtn_C from './DislikeBtn_C';
 
 
 
 class Find_C extends Component {
     render() {
         return (
-           
             <div className="App">
                 <div className="container">
                 <Nav_C/>
-                
                     <img src="http://reductress.com/wp-content/uploads/2016/10/woman-coffee-820x500.jpg" alt="Smiley face" width="100%" />
-
-
                     <div className="card mb-4">
                         <div className="card-header">
-                            {/* <button>Wink</button> */}
-
                         </div>
                         <div className="card-body">
-                            <div className="content">
+                            {/* <div className="content">
                                 <ul>
                                     <li>
-                                        <strong>Name:</strong>
+                                        <strong>name</strong> */}
                                         {/* <Link to="/Find">About</Link> */}
-                                    </li>
+                                    {/* </li>
                                     <li>
                                         <strong>Occupation:</strong>
                                     </li>
@@ -35,39 +31,104 @@ class Find_C extends Component {
                                         <strong>Location:</strong>
                                     </li>
                                 </ul>
-                            </div>
+                            </div> */}
 
                             <div className="row">
                                 <div className="col-6">
                                     {/* <button>No</button> */}
-                                    <div className="toggle">
-                                        <input type="checkbox"  />
-                                        <span className="button"></span>
-                                        <span className="label">+</span>
+                                    <div id='thumbs-down' >
+                                        <i onclick="myFunction(this)" className="fa fa-thumbs-down"></i>
+                                        <DislikeBtn_C />
                                     </div>
-
                                 </div>
                                 <div className="col-6">
                                     {/* <button>Yes</button> */}
-                                    <div className="toggle">
-                                        <input type="checkbox"  />
-                                        <span className="button"></span>
-                                        <span className="label">–</span>
+                                    <div id='heart'>
+                                    <LoveBtn_C />
                                     </div>
-
                                 </div>
-
                             </div>
                         </div>
                     </div>
-
-
                 </div>
-
             </div>
-
         );
     }
 }
 
 export default Find_C;
+
+
+// import React, { Component } from 'react';
+// import Nav_C from "./Nav_C";
+// import "./styleFind.css";
+// // import Nav_C from "./component/Nav_C";
+
+
+
+// class Find_C extends Component {
+//     render() {
+//         return (
+           
+//             <div className="App">
+//                 <div className="container">
+//                 <Nav_C/>
+                
+//                     <img src="http://reductress.com/wp-content/uploads/2016/10/woman-coffee-820x500.jpg" alt="Smiley face" width="100%" />
+
+
+//                     <div className="card mb-4">
+//                         <div className="card-header">
+//                             {/* <button>Wink</button> */}
+
+//                         </div>
+//                         <div className="card-body">
+//                             <div className="content">
+//                                 <ul>
+//                                     <li>
+//                                         <strong>Name:</strong>
+//                                         {/* <Link to="/Find">About</Link> */}
+//                                     </li>
+//                                     <li>
+//                                         <strong>Occupation:</strong>
+//                                     </li>
+//                                     <li>
+//                                         <strong>Location:</strong>
+//                                     </li>
+//                                 </ul>
+//                             </div>
+
+//                             <div className="row">
+//                                 <div className="col-6">
+//                                     {/* <button>No</button> */}
+//                                     <div className="toggle">
+//                                         <input type="checkbox"  />
+//                                         <span className="button"></span>
+//                                         <span className="label">+</span>
+//                                     </div>
+
+//                                 </div>
+//                                 <div className="col-6">
+//                                     {/* <button>Yes</button> */}
+//                                     <div className="toggle">
+//                                         <input type="checkbox"  />
+//                                         <span className="button"></span>
+//                                         <span className="label">–</span>
+//                                     </div>
+
+//                                 </div>
+
+//                             </div>
+//                         </div>
+//                     </div>
+
+
+//                 </div>
+
+//             </div>
+
+//         );
+//     }
+// }
+
+// export default Find_C;
