ba951c585affa9f10721ba89586329544407440b
Stephen Hornbeek <hornbeeks@gmail.com>
2019-02-13 21:24:05 -0800

    home page simple

--- client/src/component/Profile_C.js
+++ client/src/component/Profile_C.js
@@ -9,155 +9,162 @@ import userAPI from "../utils/userApi.js";
 import FacebookLogin from "react-facebook-login";
 
 class Profile_C extends Component {
-    state = {
-        isLoggedIn: false,
-        userID: "",
-        name: "",
-        email: "",
-        picture: "",
-        age: "",
-        description: "",
-        gender: "",
-        looking: ""
-      };
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: "",
+    age: "",
+    description: "",
+    gender: "",
+    looking: ""
+  };
 
-      handleInputChangeAge = event => {
-        this.setState({ age: event.target.value});
-      }
-      handleInputChangeDescription = event => {
-        this.setState({ description: event.target.value});
-      }
-      handleInputChangeGender = event => {
-        this.setState({ gender: event.target.value});
-      }
-      handleInputChangeLooking = event => {
-        this.setState({ looking: event.target.value})
-      }
+  handleInputChangeAge = event => {
+    this.setState({ age: event.target.value });
+  }
+  handleInputChangeDescription = event => {
+    this.setState({ description: event.target.value });
+  }
+  handleInputChangeGender = event => {
+    this.setState({ gender: event.target.value });
+  }
+  handleInputChangeLooking = event => {
+    this.setState({ looking: event.target.value })
+  }
 
-      handleFormSubmit = event => {
-        event.preventDefault();
-        console.log(userAPI.findIfUserExists(3));
-      }
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(userAPI.findIfUserExists(3));
+  }
 
-      responseFacebook = response => {
-        console.log(response);
+  responseFacebook = response => {
+    console.log(response);
+
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    });
+  };
+
+  componentClicked = () => console.log("clicked");
+
+  render() {
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = (
+        // <Profile_C>
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            // background: "#f4f4f4",
+            padding: "20px",
+            // visibility: "hidden"
+          }}
+        >
+          <img
+            style={{
+              width: "200px",
+              margin: "auto",
+              padding: "5px",
+              // visibility: "hidden"
+            }} src={this.state.picture} alt={this.state.name} />
+          <h2> {this.state.name}</h2>
+          {/* Email: {this.state.email} */}
+        </div>
+        // </Profile_C>
+      );
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="355803105005829"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
+
+    return <div>
     
-        this.setState({
-          isLoggedIn: true,
-          userID: response.userID,
-          name: response.name,
-          email: response.email,
-          picture: response.picture.data.url
-        });
-      };
+    <Nav2_C/>
+        {/* <h4>Search! Match! Video Chat!</h4> */}
+        {/* <center> <img src="https://i.ibb.co/qRmFNZB/homepagedate.png" alt="Screen-Shot-2019-02-09-at-1-19-10-PM" border="0" width="50%" /></center> */}
+       
+  <center> {fbContent} </center>
+
+      {this.state.isLoggedIn ?
       
-      componentClicked = () => console.log("clicked");
-
-    render() {
-        let fbContent;
-
-        if (this.state.isLoggedIn) {
-          fbContent = (
-            // <Profile_C>
-            <div
-              style={{
-                width: "400px",
-                margin: "auto",
-                // background: "#f4f4f4",
-                padding: "20px",
-                // visibility: "hidden"
-              }}
-            > 
-              <img 
-              style={{
-                width: "200px",
-                margin: "auto",
-                padding: "5px",
-                // visibility: "hidden"
-              }} src={this.state.picture} alt={this.state.name} />
-              <h2> {this.state.name}</h2>
-              {/* Email: {this.state.email} */}
-            </div>
-          // </Profile_C>
-          );
-        } else {
-          fbContent = (
-            <FacebookLogin
-              appId="355803105005829"
-              autoLoad={true}
-              fields="name,email,picture"
-              onClick={this.componentClicked}
-              callback={this.responseFacebook}
-            />
-          );
-        }
-    
-        return <div>
-        <Nav2_C/>
-        <center>{fbContent}</center>        
         <div className="App">
-        <div className="container">
-        {/* <FaceBookBtn_C/> */}
-        
+          <div className="container">
+            {/* <FaceBookBtn_C/> */}
 
-        
 
-        {/* <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Smiley face" width="100%" /> */}
+
+
+            {/* <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Smiley face" width="100%" /> */}
 
             <div className="card mb-4">
-           
-                <div className="card-header">
+
+              <div className="card-header">
                 {/* <center> <button>Find Love</button></center> */}
                 <center><h4>Please tell us a little more about yourself</h4></center>
 
-                </div>
-                <div className="card-body">
-                    <div className="content">
-                    <form>
-                      <div className="form-group">
-                        <label for="exampleFormControlInput1">Age</label>
-                        <input onChange={this.handleInputChangeAge} type="number" class="form-control" id="Age" placeholder="18"/>
-                      </div>
-                      <div className="form-group">
-                        <label for="exampleFormControlSelect1">Gender</label>
-                        <select onChange={this.handleInputChangeGender} className="form-control" id="Gender">
-                          <option value="" selected disabled>Please select</option>
-                          <option>Man</option>
-                          <option>Woman</option>
-                        </select>
-                      </div>
-                      <div className="form-group">
-                        <label for="exampleFormControlTextarea1">Tell us about yourself</label>
-                        <textarea onChange={this.handleInputChangeDescription} className="form-control" id="Description" rows="3"></textarea>
-                      </div>
-                      <div className="form-group">
-                        <label for="exampleFormControlSelect1">Looking for</label>
-                        <select onChange={this.handleInputChangeLooking} className="form-control" id="LookingFor">
-                        <option value="" selected disabled>Please select</option>
-                          <option>Men</option>
-                          <option>Women</option>
-                        </select>
-                      </div>
-                      <center>{this.state.isLoggedIn ? 
-                        <button type="submit" id="submit" onClick={this.handleFormSubmit}>Submit</button>
-                       : null}
-                       </center>
-                      </form>
+              </div>
+              <div className="card-body">
+                <div className="content">
+                  <form>
+                    <div className="form-group">
+                      <label for="exampleFormControlInput1">Age</label>
+                      <input onChange={this.handleInputChangeAge} type="number" class="form-control" id="Age" placeholder="18" />
                     </div>
-
-                    
-
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Gender</label>
+                      <select onChange={this.handleInputChangeGender} className="form-control" id="Gender">
+                        <option value="" selected disabled>Please select</option>
+                        <option>Man</option>
+                        <option>Woman</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlTextarea1">Tell us about yourself</label>
+                      <textarea onChange={this.handleInputChangeDescription} className="form-control" id="Description" rows="3"></textarea>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Looking for</label>
+                      <select onChange={this.handleInputChangeLooking} className="form-control" id="LookingFor">
+                        <option value="" selected disabled>Please select</option>
+                        <option>Men</option>
+                        <option>Women</option>
+                      </select>
+                    </div>
+                    <button type="submit" id="submit" onClick={this.handleFormSubmit}>Submit</button>
+                  </form>
                 </div>
+              </div>
             </div>
-            <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div>
+            {/* <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div>
+      
+                  <div class="fb-comment-embed" data-href="https://www.facebook.com/zuck/posts/10102577175875681?comment_id=1193531464007751&amp;reply_comment_id=654912701278942" data-width="560" data-include-parent="false"></div>
+                  <div class="fb-save" data-uri="https://www.instagram.com/facebook/" data-size="large"></div> */}
 
-            {/* <div class="fb-comment-embed" data-href="https://www.facebook.com/zuck/posts/10102577175875681?comment_id=1193531464007751&amp;reply_comment_id=654912701278942" data-width="560" data-include-parent="false"></div> */}
-            <div class="fb-save" data-uri="https://www.instagram.com/facebook/" data-size="large"></div>
-            
+          </div>
         </div>
-    </div></div>
-        
-    }
+        : null}
+
+
+
+
+    </div>
+
+  }
 }
 
 
